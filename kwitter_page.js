13a3f88006332bb56336a343d217ce4f263355f5
Mat1234567890 <78577538+Mat1234567890@users.noreply.github.com>
2021-11-04 21:14:24 -0400

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCLfapWdZxRLyEhaTdBcnmUDPtwPzappUI",
-    authDomain: "kwitter-f8fb7.firebaseapp.com",
-    databaseURL: "https://kwitter-f8fb7-default-rtdb.firebaseio.com",
-    projectId: "kwitter-f8fb7",
-    storageBucket: "kwitter-f8fb7.appspot.com",
-    messagingSenderId: "830041194051",
-    appId: "1:830041194051:web:8a83adb9a7543c4c775ef6"
-  };
+  apiKey: "AIzaSyCLfapWdZxRLyEhaTdBcnmUDPtwPzappUI",
+  authDomain: "kwitter-f8fb7.firebaseapp.com",
+  databaseURL: "https://kwitter-f8fb7-default-rtdb.firebaseio.com",
+  projectId: "kwitter-f8fb7",
+  storageBucket: "kwitter-f8fb7.appspot.com",
+  messagingSenderId: "830041194051",
+  appId: "1:830041194051:web:8a83adb9a7543c4c775ef6"
+};
 
 firebase.initializeApp(firebaseConfig);
 
